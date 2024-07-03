48c0f6df748bdae37ec1b026366cc6992abe755f
eunwoo <lkjin0110@gmail.com>
2024-07-03 15:38:31 +0900

    3d 시점 전환

--- core/Router.js
+++ core/Router.js
@@ -11,22 +11,41 @@ export default class Router extends Component {
 		this.$state.routes.push({ fragment, component });
 	}
 
+	// checkRoutes = () => {
+	// 	const currentRoute = this.$state.routes.find((route) => {
+	// 		return route.fragment === window.location.hash;
+	// 	});
+
+	// 	console.log(window.location.hash);
+
+	// 	if (!currentRoute) {
+	// 		window.location.href = './#';
+	// 		this.$state.routes[0].component();
+	// 	}
+
+	// 	console.log(currentRoute);
+
+	// 	currentRoute.component();
+	// }
+
 	checkRoutes = () => {
-		const currentRoute = this.$state.routes.find((route) => {
-			return route.fragment === window.location.hash;
-		});
+        const currentRoute = this.$state.routes.find((route) => {
+            return route.fragment === window.location.hash;
+        });
 
-		console.log(window.location.hash);
+        if (!currentRoute) {
+            window.location.href = './#';
+            this.$state.routes[0].component();
+        } else {
+            currentRoute.component();
+        }
 
-		if (!currentRoute) {
-			window.location.href = './#';
-			this.$state.routes[0].component();
-		}
-
-		console.log(currentRoute);
-
-		currentRoute.component();
-	}
+        // Emit a custom event with the current route
+        const event = new CustomEvent('spaNavigate', { 
+            detail: { section: currentRoute.fragment.slice(1) } 
+        });
+        document.dispatchEvent(event);
+    }
 	
 	start() {
 		window.addEventListener('hashchange', () => this.checkRoutes());
--- pages/Background.js
+++ pages/Background.js
@@ -1,22 +1,26 @@
 import Component from '../core/Component.js'
 import * as THREE from 'three';
-import * as CameraUtils from 'three/addons/utils/CameraUtils.js';
+import TWEEN from '@tweenjs/tween.js';
+
+/*
+	gltf.animations;	Array<THREE.AnimationClip>
+	gltf.scene;			THREE.Group
+	gltf.scenes;		Array<THREE.Group>
+	gltf.cameras;		Array<THREE.Camera>
+	gltf.asset;			Object
+*/
 
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { ArrowHelper } from 'three';
+
+
 
 export default class Three extends Component {
 	render() {
-		/* RENDERER */
-		const renderer = new THREE.WebGLRenderer();
-		renderer.setSize( window.innerWidth, window.innerHeight );
-		document.body.appendChild( renderer.domElement );
-		
-		/* SCENE */
+		/*** SCENE ***/
 		const scene = new THREE.Scene();
-		// scene.background = new THREE.Color( 0x333333 );
 		
-		// Load the background texture
 		const texture = new THREE.TextureLoader();
 		texture.load('../../design_src/3d/cloud.jpg', function(texture) {
 			scene.background = texture;
@@ -24,8 +28,170 @@ export default class Three extends Component {
 		
 		const lightAmbient = new THREE.AmbientLight( 0xffffff, 0.35 );
 		scene.add( lightAmbient );
+
 		
-		/* LIGHTS */
+		/*** CAMERA ***/
+		let camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 100 );
+		camera.lookAt( 0, 0, 10 );
+
+		const seeing = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 2000 );
+		seeing.position.z = 20;
+
+		let activation = camera;
+
+		/*** RENDERER ***/
+		const renderer = new THREE.WebGLRenderer({ antialias: true });
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		document.body.appendChild( renderer.domElement );
+
+
+		/*** CONTROL - Orbit ***/
+		const controls = new OrbitControls( camera, renderer.domElement );
+		// const controls = new OrbitControls( seeing, renderer.domElement );
+
+		controls.enableDamping = true;
+		controls.dampingFactor = 0.05;
+		
+		controls.screenSpacePanning = false;
+		
+		controls.minDistance = 0;
+		controls.maxDistance = 1; 
+		controls.minPolarAngle = Math.PI / 3;
+		controls.maxPolarAngle = Math.PI / 1.5;
+
+
+		/*** CONTROL - Tween ***/
+		const cameraPositions = {
+			"/": { x: 0.001, y: 0.001, z: 0.001 },
+			"lobby/": { x: 0.001, y: 0.001, z: 4.9 }
+		};
+
+		function onSPANavigation( event ) {
+			const section = event.detail.section; 
+			updateCameraForSection( section );
+		}
+
+		// function navigateToSection( section ) {
+		// 	updateCameraForSection( section );
+		// 	// Your SPA navigation logic here
+		// }
+		
+		function updateCameraForSection( section ) {
+			const targetPosition = cameraPositions[section];
+
+			console.log( "in section: " + section );
+
+			if ( targetPosition ) {
+				if ( section === "/" ) { setCameraHome(); }
+				else { setCameraLobby(); }
+
+				animateCamera( targetPosition );
+				console.log( camera.position );
+			}
+		}
+
+		function animateCamera( targetPosition, duration = 2300 ) {
+			new TWEEN.Tween( camera.position )
+				.to( targetPosition, duration )
+				.easing( TWEEN.Easing.Quadratic.InOut )
+		
+				// .onComplete(() => {
+				// 	camera.position.x = Math.round(camera.position.x * 100) / 100;
+				// 	camera.position.y = Math.round(camera.position.y * 100) / 100;
+				// 	camera.position.z = Math.round(camera.position.z * 100) / 100;
+				// 	console.log("Rounded camera position after tween:", camera.position);
+				// })
+
+				.start();
+				
+			new TWEEN.Tween( controls.target )
+				.to( {
+					x: targetPosition.x,
+					y: targetPosition.y,
+					z: targetPosition.z + 0.1
+				},
+				duration)
+				.easing( TWEEN.Easing.Quadratic.InOut )
+				.onUpdate(() => controls.update())
+				.start();
+		}
+
+		function setCameraHome() {
+			camera.fov = 50;
+			controls.enabled = true;
+		}
+
+		function setCameraLobby() {
+			camera.fov = 15;
+			controls.enabled = false;
+		}
+
+		document.addEventListener( 'spaNavigate', onSPANavigation );
+
+
+		/*** LISTEN ***/
+		window.addEventListener( 'resize', onWindowResize );
+		// document.addEventListener( 'hashchange', onSPANavigation );
+		document.addEventListener( 'keydown', onKeyDown );
+		
+		function onWindowResize() {
+			camera.aspect = window.innerWidth / window.innerHeight;
+			camera.updateProjectionMatrix();
+			
+			renderer.setSize( window.innerWidth, window.innerHeight );
+		}
+
+		// window.addEventListener( 'resize', () => {
+		// 	camera.aspect = window.innerWidth / window.innerHeight;
+		// 	camera.updateProjectionMatrix();
+		// 	renderer.setSize( window.innerWidth, window.innerHeight );
+		// });
+
+		function onKeyDown( event ) {
+			switch ( event.keyCode ) {
+				case 80:
+
+					if ( activation === camera ) {
+						activation = seeing;
+						helper.visible = true;
+						// arrow.visible = true;
+						arrowWorld.visible = true;
+						controls.object = seeing;
+					}
+
+					else {
+						activation = camera;
+						helper.visible = false;
+						// arrow.visible = false;
+						arrowWorld.visible = false;
+						controls.object = camera;
+					}
+
+					break;
+			}
+		}
+
+
+		
+		
+		/*** WORLD - Asset ***/
+		const loader = new GLTFLoader();
+		
+		loader.load(
+			'../../design_src/3d/school_class_room/scene.gltf',
+		
+			function ( gltf ) {
+		
+				gltf.scene.position.set( 96.55, -1.55, -13 );
+				scene.add( gltf.scene );
+		
+			},
+			function (xhr) { console.log((xhr.loaded / xhr.total * 100) + '% loaded'); },
+			function (error) { console.error('An error happened', error); }
+		);
+
+
+		/*** WORLD - Lights ***/
 		const frontLeft = new THREE.DirectionalLight( 0xffffff, 0.9 );
 		const frontMid = new THREE.DirectionalLight( 0xffffff, 0.9 );
 		const frontRight = new THREE.DirectionalLight( 0xffffff, 0.9 );
@@ -50,96 +216,85 @@ export default class Three extends Component {
 		const hemi = new THREE.HemisphereLight( 0xffffff, 0xffffff, 1.2 );
 		hemi.position.set( 0, 0, 0 );
 		scene.add( hemi );
+
+
+		/*** WORLD - Helper ***/
+		const helper = new THREE.CameraHelper( camera );
+		helper.visible = false;
+		scene.add( helper );
 		
 		// const hemiHelper = new THREE.HemisphereLightHelper( hemi, 5 );
 		// scene.add( hemiHelper );
+
+		const arrowWorld = new THREE.AxesHelper( 2 );
+		arrowWorld.visible = false;
+		scene.add( arrowWorld );
+
+		// function createArrow( camera ) {
+		// 	const axesHelper = new THREE.Group();
 		
-		/* CAMERA */
-		let camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 2000 );
-		camera.position.set( 0, 0, 0.1 );
+		// 	const length = 1;
 		
-		let cameraRig = new THREE.Group();
-		cameraRig.add( camera );
+		// 	const xArrow = new THREE.ArrowHelper(
+		// 		new THREE.Vector3(1, 0, 0),
+		// 		new THREE.Vector3(0, 0, 0),
+		// 		length,
+		// 		0x0000ff
+		// 	);
+		// 	axesHelper.add(xArrow);
 		
-		// cameraRig.position.set( 0, 0, 0 );
-		cameraRig.lookAt( 0, 0, -1 );
+		// 	const yArrow = new THREE.ArrowHelper(
+		// 		new THREE.Vector3(0, 1, 0),
+		// 		new THREE.Vector3(0, 0, 0),
+		// 		length,
+		// 		0x00ff00
+		// 	);
+		// 	axesHelper.add(yArrow);
 		
-		scene.add( cameraRig );
+		// 	const zArrow = new THREE.ArrowHelper(
+		// 		new THREE.Vector3(0, 0, 1),
+		// 		new THREE.Vector3(0, 0, 0),
+		// 		length,
+		// 		0xff0000
+		// 	);
+		// 	axesHelper.add(zArrow);
 		
-		// const helper = new THREE.CameraHelper( camera );
-		// scene.add( helper );
+		// 	axesHelper.visible = false;
+		// 	scene.add(axesHelper);
 		
-		const seeing = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 10000 );
-		seeing.position.z = 2500;
-		
-		/* CONTROL */
-		const controls = new OrbitControls( camera, renderer.domElement );
-		// const controls = new OrbitControls( seeing, renderer.domElement );
-		
-		controls.listenToKeyEvents( window ); // optional
-		
-		controls.enableDamping = true;
-		controls.dampingFactor = 0.05;
-		
-		controls.screenSpacePanning = false;
-		
-		controls.minDistance = 0;
-		controls.maxDistance = 1; 
-		controls.minPolarAngle = Math.PI / 3;
-		controls.maxPolarAngle = Math.PI / 1.5;
+		// 	return axesHelper;
+		// }
+
+		// const arrow = createArrow( camera );
 		
 		
-		/* LOAD ASSET */
-		const loader = new GLTFLoader();
-		
-		// gltf.animations; // Array<THREE.AnimationClip>
-		// gltf.scene; // THREE.Group
-		// gltf.scenes; // Array<THREE.Group>
-		// gltf.cameras; // Array<THREE.Camera>
-		// gltf.asset; // Object
-		
-		// Load a glTF resource
-		loader.load(
-			// resource URL
-			'../../design_src/3d/school_class_room/scene.gltf',
-		
-			// called when the resource is loaded
-			function ( gltf ) {
-		
-				gltf.scene.position.set( 96.55, -1.55, -13 );
-		
-				scene.add( gltf.scene );
-		
-			},
-			function (xhr) { console.log((xhr.loaded / xhr.total * 100) + '% loaded'); },
-			function (error) { console.error('An error happened', error); }
-		);
-		
-		window.addEventListener( 'resize', onWindowResize );
-		
-		function onWindowResize() {
-			camera.aspect = window.innerWidth / window.innerHeight;
-			camera.updateProjectionMatrix();
+		/*** SHOOT ***/
+		function animate() {
 			
-			renderer.setSize( window.innerWidth, window.innerHeight );
+			TWEEN.update();
+			controls.update();
+			camera.updateMatrixWorld();
+			
+			// arrow.position.copy( camera.position );
+			// arrow.quaternion.copy( camera.quaternion );
+			
+			render();
+
 		}
 		
-		// function onPageLobby() {
-			// 	camera.
-			// }
-			
-			/* RENDERING */
-			function animate() {
-				controls.update();
-				
-				render();
-			}
-			
-			function render() {
-				renderer.render( scene, camera );
-				// renderer.render( scene, seeing );
-			}
-			
-			renderer.setAnimationLoop( animate );
+		function render() {
+			renderer.render( scene, activation );
+			// renderer.render( scene, camera );
+			// renderer.render( scene, seeing );
 		}
+		
+		/*
+		 *	The renderer.setAnimationLoop(animate) call is
+		 *	Three.js's way of using requestAnimationFrame internally,
+		 *	which is equivalent to calling requestAnimationFrame(animate) directly.
+		 */
+
+		// requestAnimationFrame( animate );
+		renderer.setAnimationLoop( animate );
 	}
+}
